9e4e519d893ea2db3ecc87396f9f2582798f7d3f
trose618 <trose618@gmail.com>
2018-10-30 18:56:02 -0400

    returns film info for characters on multiple pages

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,36 +5,15 @@ require 'pry'
 def get_character_movies_from_api(character)
   #make the web request
   movies = []
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
-  # NOTE: in this demonstration we name many of the variables _hash or _array.
-  # This is done for educational purposes. This is not typically done in code.
-
   # iterate over the response hash to find the collection of `films` for the given
-    response_hash["results"].each_with_index do |a_character, index|
-      if a_character["name"].downcase == character
-        film_array = response_hash["results"][index]["films"]
-        film_array.each do |movie_link|
-          resp_hash = JSON.parse(RestClient.get(movie_link))
-          movies << resp_hash["title"]
-          end
-        end
-      end
-
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  get_character_info(character)["films"].each do |link|
+    resp_hash = JSON.parse(RestClient.get(link))
+    movies << resp_hash["title"]
+    end
   movies
 end
 
 def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
   films_hash.each_with_index do |movie, index|
     puts "#{index + 1} #{movie}"
   end
@@ -52,3 +31,46 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+#method that returns the response hash #done
+def response_hash
+  JSON.parse(RestClient.get('http://www.swapi.co/api/people/'))
+end
+
+#method that takes character name and return character's info
+def get_character_info(character)
+  character_info = {}
+  character_info_array.each do |a_character|
+    if a_character["name"].downcase == character
+      character_info = a_character
+    end
+  end
+  character_info
+end
+
+#method that takes character name and attr and returns it's data
+def get_character_attr(character, attr)
+  get_character_info(character)[attr] #<< accessing hash
+end
+
+#gathers character info from all pages on SWAPI into an array and returns that array
+def character_info_array
+  puts "\nGathering information, just a sec..."
+  puts "*" * 10
+
+  array = []
+  array << response_hash["results"]
+  array_of_character_info = []
+
+resp_hash = response_hash
+  while resp_hash["next"]
+      new_link = resp_hash["next"]
+      resp_hash = JSON.parse(RestClient.get(new_link))
+      array << resp_hash["results"]
+  end
+  array.each do |page|
+    array_of_character_info.concat(page)
+  end
+  array_of_character_info
+end
+#make an array containing all character hashes
